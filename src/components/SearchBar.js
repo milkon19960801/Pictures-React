0d110a27f161998ba462c2da374baebf39459ed5
milkon <melkon_2010@hotmail.com>
2021-05-05 15:30:52 +0200

    recatoring App component to class component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,16 +3,21 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
+  onFromSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
-    console.log(this.state);
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFromSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
               value={this.state.term}
+              placeholder="Type something..."
               onChange={(e) => this.setState({ term: e.target.value })}
             />
           </div>
