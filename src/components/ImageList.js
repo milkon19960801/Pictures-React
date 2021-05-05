a4520ce11b76214e2affacd2e3d14a21e8107edb
milkon <melkon_2010@hotmail.com>
2021-05-05 17:57:43 +0200

    redering images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,11 @@
 import React from "react";
 
 const ImageList = (props) => {
+  const images = props.images.map((image) => {
+    return <img src={image.urls.regular} alt={image.alt_description} />;
+  });
   console.log(props.images);
-  return <div></div>;
+  return <div>{images}</div>;
 };
 
 export default ImageList;
