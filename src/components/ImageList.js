9bc1dc137fc7de9040949f3b1d4f2943e13843ba
milkon <melkon_2010@hotmail.com>
2021-05-06 12:45:06 +0200

    css grid to show list elements in grid layout

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
+import "./ImageList.css";
 import React from "react";
 
 const ImageList = (props) => {
   const images = props.images.map(({ urls, alt_description, id }) => {
     return <img src={urls.regular} alt={alt_description} key={id} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
